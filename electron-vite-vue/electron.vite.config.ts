e505405a2f0c18c8cfd96655552288c0048e7d8e
agamba <lodeanto@gmail.com>
2023-08-28 16:59:47 -0500

    added vue and react examples

---
+++ electron-vite-vue/electron.vite.config.ts
@@ -0,0 +1,27 @@
+import { resolve } from 'path'
+import { defineConfig, externalizeDepsPlugin, bytecodePlugin } from 'electron-vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin(), bytecodePlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin(), bytecodePlugin()]
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src')
+      }
+    },
+    plugins: [vue()]
+  }
+})
+
+// import { defineConfig } from 'vite'
+// import preact from '@preact/preset-vite'
+
+// export default defineConfig({
+//   plugins: [preact()]
+// })
